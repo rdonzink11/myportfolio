9aade945c49b039b4c92356604e8afa2018e0d9d
rdonzink11 <rdonzink11@gmail.com>
2023-02-27 10:51:10 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -22,14 +22,22 @@ showMenu = false;
 
 const fullImgbox = document.getElementById ("fullImgBox")
 const fullImg =document.getElementById ("fullImg")
+const imgText = document.getElementById ("imgText")
 
 function openFullImg(pic){
         fullImgbox.style.display = "flex";
-        fullImg.src = pic;
+        fullImg.src = pic.src;
+        imgText.innerHTML = pic.alt;
+
+
 }
 
 function closeFullImg(){
 
     fullImgbox.style.display = "none";            
 };
+
+
+
+
 
